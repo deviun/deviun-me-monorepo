8964e057fa51c7f993e56006ebf00d78e33fe558
deviun <a.deviun@gmail.com>
2019-06-23 16:15:37 +0300

    add env handler

--- deviun-me/next.config.js
+++ deviun-me/next.config.js
@@ -1,2 +1,11 @@
+require('dotenv').config();
 const withTypescript = require('@zeit/next-typescript');
-module.exports = withTypescript();
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(process.env)
+    )
+    return config
+  },
+  ...withTypescript()
+};
