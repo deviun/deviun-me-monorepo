e7f5ac83e127b47fdabea3620d01c747fe33f57c
deviun <a.deviun@gmail.com>
2019-06-29 19:31:02 +0300

    skills list styles

--- deviun-me/pages/components/developer/SkillsList.tsx
+++ deviun-me/pages/components/developer/SkillsList.tsx
@@ -18,7 +18,7 @@ const SkillLevelStyled = styled.div`
   }) => `
     margin: 50px 0;
     position: relative;
-    display: block;
+    display: inline-block;
     color: #${props.color};
     width: 0px;
     height: 0px;
@@ -26,6 +26,8 @@ const SkillLevelStyled = styled.div`
     border-bottom: 70px solid #${props.color};
     border-left: 100px solid transparent;
     transform: rotate(35deg) scale(0.15, 0.15);
+    transition: 0.1s;
+    opacity: 0.3;
 
     &:before {
       border-bottom: 80px solid #${props.color};
@@ -58,22 +60,54 @@ const SkillLevelStyled = styled.div`
   `}
 `;
 
-const SkillWrapperStyled = styled.div`
+const SkillsWrapper = styled.div`
+  text-align: center;
+`;
+
+const SkillItem = styled.div`
   padding: 10px;
   display: inline-block;
+  border-radius: 36px;
+  transition: 0.1s;
+
+  img {
+    transition: 0.1s;
+    max-height: 150px;
+    margin: 20px;
+    vertical-align: middle;
+  }
+
+  .skill-level {
+    vertical-align: middle;
+  }
+
+  &:hover {
+    background: #F7F7F7;
+    box-shadow: 0 2px 4px 0 rgba(200,200,200,0.50);
+    transform: scale(1.05, 1.05);
+
+    img {
+      opacity: 0.9;
+    }
+
+    .skill-level {
+      transform: rotate(35deg) scale(0.3, 0.3);
+      opacity:1;
+    }
+  }
 `;
 
 export default function SkillsList(props: SkillsListPropsT) {
   return (
-    <>
+    <SkillsWrapper>
       {
         props.data.map((skill) => (
-          <SkillWrapperStyled>
+          <SkillItem>
             <img src={skill.imagePath} />
-            <SkillLevelStyled color={skillLevels[skill.level]} />
-          </SkillWrapperStyled>
+            <SkillLevelStyled className="skill-level" color={skillLevels[skill.level]} />
+          </SkillItem>
         ))
       }
-    </>
+    </SkillsWrapper>
   );
 }
