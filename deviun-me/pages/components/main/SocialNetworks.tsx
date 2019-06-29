f5bae7c843bb0fe4cda81756f3f14610bb54452e
deviun <a.deviun@gmail.com>
2019-06-29 18:37:54 +0300

    add links tooltips

--- deviun-me/pages/components/main/SocialNetworks.tsx
+++ deviun-me/pages/components/main/SocialNetworks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 
@@ -11,6 +11,7 @@ export interface SocialNetworkItemT {
   link: string;
   target?: string;
   internal?: boolean;
+  title: string;
 }
 
 export interface SocialNetworksPropsT {
@@ -37,29 +38,78 @@ const SocialNetworkStyled = styled.div`
   }
 `;
 
+interface ActiveTitleStyledPropsT {
+  show: boolean | number;
+}
+
+const ActiveTitleStyled = styled.div`
+  ${(props: ActiveTitleStyledPropsT) => `
+    opacity: ${ props.show ? '1' : '0' };
+  `}
+  margin: 20px;
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  margin-top: -50px;
+  text-align: center;
+  transition: 0.5s;
+
+  span {
+    background: black;
+    color: white;
+    padding: 15px;
+    border-radius: 100px;
+    font-size: 14px;
+  }
+`;
+
+const LinkWrapper = styled.div`
+  display: inline-block;
+`;
+
 export default function SocialNetworks({
   items = [],
 }: SocialNetworksPropsT) {
+  const [activeTitle, setActiveTitle] = useState('');
+  const [showTitle, setShowTitle] = useState(false);
+
   return (
     <SocialNetworkStyled>
+      <ActiveTitleStyled
+        show={showTitle}
+      >
+        <span>
+          {activeTitle}
+        </span>
+      </ActiveTitleStyled>
       {
         items.map((item) => (
-          item.internal
-            ? (
-              <Link
-                href={item.link}
-              >
-                <img src={item.svgPath} />
-              </Link>
-            )
-            : (
-              <a
-                href={item.link}
-                target={item.target || '_blank'}
-              >
-                <img src={item.svgPath} />
-              </a>
-            )
+          <LinkWrapper
+            onMouseOver={() => {
+              setActiveTitle(item.title);
+              setShowTitle(true);
+            }}
+            onMouseOut={() => setShowTitle(false)}
+          >
+            {
+              item.internal
+                ? (
+                  <Link
+                    href={item.link}
+                  >
+                    <img src={item.svgPath} />
+                  </Link>
+                )
+                : (
+                  <a
+                    href={item.link}
+                    target={item.target || '_blank'}
+                  >
+                    <img src={item.svgPath} />
+                  </a>
+                )
+            }
+          </LinkWrapper>
         ))
       }
     </SocialNetworkStyled>
