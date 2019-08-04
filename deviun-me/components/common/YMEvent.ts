7d04e5dfc5d308b274ea88539414efbe9d6bce9d
deviun <a.deviun@gmail.com>
2019-08-04 21:59:00 +0300

    ignore YM events on development mode

--- deviun-me/components/common/YMEvent.ts
+++ deviun-me/components/common/YMEvent.ts
@@ -2,11 +2,12 @@
 const { ym } = typeof window !== 'undefined'
   ? window
   : {};
+const isProd = process.env.NODE_ENV === 'production';
 
 const YMID = 50268295;
 
 export default function event(eventName) {
-  if (!ym) {
+  if (!ym || !isProd) {
     return null;
   }
   
